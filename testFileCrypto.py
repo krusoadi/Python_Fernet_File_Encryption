0726e877336253ef8b4570954608139005cad12b
krusoadi <krusoadam@gmail.com>
2024-05-25 15:30:00 +0200

    runned tests, okay

--- testFileCrypto.py
+++ testFileCrypto.py
@@ -1,8 +1,10 @@
 from FileCrypto import FileCrypto
 import lorem
 
+n = 1000
+
 with open("asd.txt", "w") as out:
-    for i in range(1000):
+    for i in range(n):
         out.write(lorem.paragraph())
 
 file = FileCrypto("asd.txt")
@@ -12,3 +14,12 @@ file.encrypt(500)
 file2 = FileCrypto("asd_encrypted.txt")
 
 file2.decrypt("final.txt")
+
+
+with open("final.txt","r") as final, open("asd.txt", "r") as original:
+    for i in range(len(lorem.paragraph())*n):
+        if final.read(1) != original.read(1):
+            print("\nnot ")
+            break
+        
+    print("ok\n")
