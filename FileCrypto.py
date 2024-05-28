1cadf3b82319f101ff5b01cd35d62db360f99654
krusoadi <krusoadam@gmail.com>
2024-05-28 18:38:52 +0200

    Commenting FileCrypto

--- FileCrypto.py
+++ FileCrypto.py
@@ -1,124 +1,141 @@
 from os import path
 from math import floor
 from cryptography.fernet import Fernet
-from io import BufferedReader
-from io import BufferedWriter
-from ttkbootstrap import DoubleVar
+from typing import BinaryIO
+
 
 class FileCrypto:
-    def __init__(self, fileNameIn:str, keyFileName:str = "secret.key") -> None:
-        self.fileName, self.fileExtension = path.splitext(fileNameIn)
-        self.keyFileName:str = keyFileName
-        self.size: int = path.getsize(fileNameIn) # Normal file reading size
-        self.last_size:int = None # Normal file reading remainder size
-        
-        self.reading_unit:int = None
-        
-        self.Fernet:Fernet = None
-        self.key:bytes = None
-        
-        self.encryptedChunkSize:int = None # Encrypted file write size
-        self.lastEncryptedChunkSize:int = None #Encrypted file write size
-        
-        self.progress = 0 # for the UI progressbar
-        self.progressMax = 1 
-        
+    """FileCrypto is a class for encrypting files using Fernet continuously, with a given reading unit.\n
+    Unfortunately Fernet is only capable of decrypting files if we use the same chunk size which it encrypted our
+    reading units to. This class manages these problems automatically with saving the encrypted chunks sizes and
+    decrypting it with them."""
+    def __init__(self, file_name: str, key_file: str = "secret.key") -> None:
+        self.fileName, self.fileExtension = path.splitext(file_name)
+        self.keyFileName: str = key_file
+        self.size: int = path.getsize(file_name)  # Normal file reading size
+        self.last_size: int | None = None  # Normal file reading remainder size
 
-    def _generateKey(self) -> None:
+        self.reading_unit: int | None = None
+
+        self.Fernet: Fernet | None = None
+        self.key: bytes | None = None
+
+        self.encryptedChunkSize: int | None = None  # Encrypted file write size
+        self.lastEncryptedChunkSize: int | None = None  # Encrypted file write size
+
+        self.progress = 0  # for the UI progressbar
+        self.progressMax = 1
+
+    def _generate_key(self) -> None:
+        """Private method for generating a Fernet key"""  # TODO check if it's random enough
         self.key = Fernet.generate_key()
-    
-    def _setGenerator(self) ->None:
+
+    def _set_generator(self) -> None:
+        """Private method for constructing the Fernet Client with the chosen key."""
         self.Fernet = Fernet(key=self.key)
-    
-    def _retrieveKeyFileData(self):
-        with open(self.keyFileName, "rb") as kfile:
-            keysize = path.getsize(self.keyFileName) - 8
-            self.encryptedChunkSize = int.from_bytes(kfile.read(4), "big")
-            self.lastEncryptedChunkSize = int.from_bytes(kfile.read(4), "big")
-            self.key = kfile.read(keysize)
 
-    def _writeKeyFileData(self):
-        with open(self.keyFileName, "wb") as kfile:
-            kfile.write(self.encryptedChunkSize.to_bytes(4, "big"))
-            kfile.write(self.lastEncryptedChunkSize.to_bytes(4, "big"))
-            kfile.write(self.key)            
+    def _retrieve_key_file_data(self):
+        """Private method for opening the chosen key file for reading in the key, and the chunk sizes."""
+        with open(self.keyFileName, "rb") as key_file:
+            key_size = path.getsize(self.keyFileName) - 8
+            self.encryptedChunkSize = int.from_bytes(key_file.read(4), "big")
+            self.lastEncryptedChunkSize = int.from_bytes(key_file.read(4), "big")
+            self.key = key_file.read(key_size)
 
-    def _readData(self, buffer: BufferedReader) ->bytes:
+    def _write_key_file_data(self):
+        """Private method for saving the key and the chunk sizes in the key file."""
+        with open(self.keyFileName, "wb") as key_file:
+            key_file.write(self.encryptedChunkSize.to_bytes(4, "big"))
+            key_file.write(self.lastEncryptedChunkSize.to_bytes(4, "big"))
+            key_file.write(self.key)
+
+    def _read_data(self, buffer: BinaryIO) -> bytes:
+        """Private Wrapper method for reading data from a given file with the reading_unit."""
         return buffer.read(self.reading_unit)
-            
-    def _readLastData(self, buffer: BufferedReader) -> bytes:
+
+    def _read_last_data(self, buffer: BinaryIO) -> bytes:
+        """Private Wrapper method for reading the last data from a given file."""
         return buffer.read(self.last_size)
 
-    def _readEncryptedData(self, buffer: BufferedReader) -> bytes:
+    def _read_encrypted_data(self, buffer: BinaryIO) -> bytes:
+        """Private Wrapper method for reading encrypted data chunks from a given file."""
         return buffer.read(self.encryptedChunkSize)
 
-    def _readLastEncryptedData(self, buffer: BufferedReader) -> bytes:
+    def _read_last_encrypted_data(self, buffer: BinaryIO) -> bytes:
+        """Private Wrapper method for reading the last encrypted data chunks from a given file."""
         return buffer.read(self.lastEncryptedChunkSize)
 
-    def _writeData(self, data:bytes , buffer: BufferedWriter) -> None:
+    @staticmethod
+    def _write_data(data: bytes, buffer: BinaryIO) -> None:
+        """Private Wrapper method for writing data to a file."""
         buffer.write(data)
-        
-    def _fetchDecryptAndThrow(self, ibuffer: BufferedReader, obuffer:BufferedWriter, last:bool) -> None:
+
+    def _fetch_decrypt_and_throw(self, i_buffer: BinaryIO, o_buffer: BinaryIO, last: bool) -> None:
+        """This private method reads data from the given input file (i_buffer), decrypts it and writes out to the given
+        output (o_buffer)"""
         if last:
-            data = self._readLastEncryptedData(ibuffer) 
+            data = self._read_last_encrypted_data(i_buffer)
         else:
-            data = self._readEncryptedData(ibuffer)
+            data = self._read_encrypted_data(i_buffer)
         decrypted = self.Fernet.decrypt(data)
-        self._writeData(decrypted, obuffer)
-    
-    def _fetchEncryptAndThrow(self, ibuffer: BufferedReader, obuffer:BufferedWriter, last :bool) -> None:
+        self._write_data(decrypted, o_buffer)
+
+    def _fetch_encrypt_and_throw(self, i_buffer: BinaryIO, o_buffer: BinaryIO, last: bool) -> None:
+        """This private method reads in raw data from the input (i_buffer), encrypts it, and writes out to the output
+        (o_buffer), and saves the encrypted chunk size for the key file"""
         if last:
-            data = self._readLastData(ibuffer) 
-            encrypted = self.Fernet.encrypt(data)    
+            data = self._read_last_data(i_buffer)
+            encrypted = self.Fernet.encrypt(data)
             self.lastEncryptedChunkSize = len(encrypted)
         else:
-            data = self._readData(ibuffer)
+            data = self._read_data(i_buffer)
             encrypted = self.Fernet.encrypt(data)
             self.encryptedChunkSize = len(encrypted)
-    
-        self._writeData(encrypted, obuffer)
-        
-    def encrypt(self, reading_unit:int) -> None:
-        self._generateKey()
-        self._setGenerator()
+
+        self._write_data(encrypted, o_buffer)
+
+    def encrypt(self, reading_unit: int) -> None:
+        """Public method for continuously encrypting a file, with the given reading_unit."""
+
+        self._generate_key()
+        self._set_generator()
         self.reading_unit = reading_unit
-        
-        
-        with open(self.fileName+self.fileExtension, "rb") as rawFile, open(self.fileName+"_encrypted"+self.fileExtension, "wb") as out:
+
+        with open(self.fileName + self.fileExtension, "rb") as rawFile, open(
+                self.fileName + "_encrypted" + self.fileExtension, "wb") as out:
             iterations = floor(self.size / reading_unit)
             self.last_size = self.size - (iterations * reading_unit)
             self.progressMax = iterations - 1
-            
+
             if self.size < reading_unit:
                 self.progressMax = 1
-                self._fetchEncryptAndThrow(rawFile, out, True)
+                self._fetch_encrypt_and_throw(rawFile, out, True)
                 self.encryptedChunkSize = self.lastEncryptedChunkSize
                 self.progress += 1
-                    
+
             else:
                 for i in range(iterations + 1):
-                    self._fetchEncryptAndThrow(rawFile, out, i == iterations)
+                    self._fetch_encrypt_and_throw(rawFile, out, i == iterations)
                     self.progress += 1
-            
-            self._writeKeyFileData()
-                   
-    def decrypt(self, newName:str) -> None:
-        self._retrieveKeyFileData()
-        self._setGenerator()
+
+            self._write_key_file_data()
+
+    def decrypt(self, new_name: str) -> None:
+        """Public method for continuously decrypting a file and saving the data in the new_name file."""
+        self._retrieve_key_file_data()
+        self._set_generator()
         self.reading_unit = self.encryptedChunkSize
-        
+
         iterations = floor(self.size / self.reading_unit)
-        
+
         self.progressMax = iterations - 1
-        
-        with open(self.fileName+self.fileExtension, "rb") as encrypted, open(newName, "wb") as rawFile:
+
+        with open(self.fileName + self.fileExtension, "rb") as encrypted, open(new_name, "wb") as rawFile:
             if self.size == self.reading_unit:
                 self.progressMax = 1
-                self._fetchDecryptAndThrow(encrypted, rawFile, True)
-            
+                self._fetch_decrypt_and_throw(encrypted, rawFile, True)
+
             else:
                 for i in range(iterations + 1):
-                    self._fetchDecryptAndThrow(encrypted, rawFile, i==iterations)
+                    self._fetch_decrypt_and_throw(encrypted, rawFile, i == iterations)
                     self.progress += 1
-                    
-            
--- OOP.py
+++ OOP.py
@@ -4,14 +4,15 @@ import ttkbootstrap as ttk
 from tkinter.filedialog import askopenfilename
 from os.path import dirname
 
+
 class App(ttk.Window):
-    def __init__(self, themename:str ="darkly") -> None:
-        super().__init__(themename=themename)
-        
+    def __init__(self, theme: str = "darkly") -> None:
+        super().__init__(themename=theme)
+
         self.title("MyPc")
         self.geometry('1500x1500')
         self.file = None
-    
+
         self.button = ttk.Button(style="success", text="Encrypt", command=self.encrypt_file)
         self.button.pack()
 
@@ -19,34 +20,34 @@ class App(ttk.Window):
         self.button.pack()
 
         self.progress = ttk.DoubleVar(self, 0)
-        self.progressBar =ttk.Progressbar(master=self, variable=self.progress, maximum=10, style="success", length=400)
+        self.progressBar = ttk.Progressbar(master=self, variable=self.progress, maximum=10, style="success", length=400)
         self.progressBar.pack()
-                  
+
     def setFileName(self):
         Filename = askopenfilename()
         Keyfile = dirname(Filename) + "/fernet.key"
-        
+
         self.file = FileCrypto(Filename, Keyfile)
-    
+
     def update_progress(self):
-        self.progress.set((self.file.progress/self.file.progressMax)*100)
+        self.progress.set((self.file.progress / self.file.progressMax) * 100)
         if self.file.progress < self.file.progressMax:
             self.after(500, self.update_progress)
 
     def encrypt_file(self):
         self.progress.set(0)
         self.setFileName()
-        
+
         threading.Thread(target=self.file.encrypt, args=(100,), daemon=True).start()
         self.after(500, self.update_progress)
-        
+
     def decrypt_file(self):
         self.progress.set(0)
         self.setFileName()
-        
+
         threading.Thread(target=self.file.decrypt, args=("final.txt",), daemon=True).start()
         self.after(500, self.update_progress)
-        
+
 
 app = App()
 
